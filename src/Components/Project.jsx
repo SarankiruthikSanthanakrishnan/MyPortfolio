32a87f5f784605edb384c6d4ba0d20d3e48c7a18
SarankiruthikSanthanakrishnan <skiruthik2510@gmail.com>
2025-08-02 17:14:37 +0530

    All Details

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,8 +1,7 @@
-import {useEffect} from 'react'
-import projectsData from '../assets/Product.json'
+import { useEffect } from 'react';
+import projectsData from '../assets/Product.json';
 import ProjectCard from './ProjectCard';
-
-
+import './Project.css'; // Make sure to create and link this CSS file
 
 const Project = () => {
   useEffect(() => {
@@ -10,20 +9,25 @@ const Project = () => {
   }, []);
 
   return (
-    <>
-    <div className="card">
-      <div className="card-body">
-        <div className="row d-flex mx-auto my-auto justify-content-center align-items-center">
+    <section className="projects-container py-5">
+      <div className="container-fluid">
+        {/* Main Heading for the Projects Section */}
+        <h1 className="text-center display-4 fw-bold mb-5 text-white">
+          <i className="bi bi-code-slash text-primary"></i> My Projects
+        </h1>
+
+        <div className="row d-flex justify-content-center align-items-center">
           <div className="col col-xxl-10 col-xl-10 col-lg-10 col-md-10 col-sm-11 col-xs-12">
             {projectsData.map((project, index) => (
-              <ProjectCard key={index} project={project} />
+              <div key={index} className="project-card-wrapper mb-4">
+                <ProjectCard project={project} />
+              </div>
             ))}
           </div>
         </div>
       </div>
-    </div>
-    </>
-  )
-}
+    </section>
+  );
+};
 
-export default Project
+export default Project;
--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const ProjectCard = ({ project }) => {
   return (
     <div className="card-body1 my-4">
-      <table className="table text-center table-borderless">
+      <table className="table text-center table-borderless text-light">
         <tbody>
           <tr>
             <td colSpan="3" className="text-center">
